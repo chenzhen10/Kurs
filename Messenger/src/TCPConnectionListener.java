a25e304eda930de5d2f958724f59f51b050c89bb
Timofei Kimbar <teekeen32@gmail.com>
2018-12-16 15:29:52 +0300

    All done

--- Messenger/src/TCPConnectionListener.java
+++
@@ -1,6 +0,0 @@
-public interface TCPConnectionListener {
-    void onConnectionReady(TCPConnection tcpConnection);
-    void onRecieveString(TCPConnection tcpConnection,String value);
-    void onDisconnect(TCPConnection tcpConnection);
-    void onException(TCPConnection tcpConnection ,Exception e);
-}
